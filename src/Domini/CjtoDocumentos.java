1c4184ca28b29dbf2451db5da76ab5f6eb8486da
jessicasobreviela <jessi.sobreviela@gmail.com>
2016-10-27 20:40:57 +0200

    falta arreglar y aclarar ideas

--- src/Domini/CjtoDocumentos.java
+++ src/Domini/CjtoDocumentos.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 public class CjtoDocumentos {
     private DocList vecDocumentos;
     private int id;
+    private int numDocs;
     private ArrayList<String> español;
     private ArrayList<String> catalan;
     private ArrayList<String> ingles;
@@ -24,6 +25,7 @@ public class CjtoDocumentos {
     public CjtoDocumentos() throws IOException, FileNotFoundException{
         vecDocumentos = new DocList();
         id = 0;
+        numDocs = 0;
         CtrlDomini ctrlDomini = new CtrlDomini();
         español = new ArrayList<>();
         catalan = new ArrayList<>();
@@ -31,16 +33,43 @@ public class CjtoDocumentos {
         ctrlDomini.leerPalabras(español, catalan, ingles);
     }
     
-    public void altaDocumento(String autor, String titulo, String contenido) throws ExceptionDomini{
+    public ArrayList<Titulo> consultarTitulosAutor(Autor autor){
+        
+    }
+    
+    public ArrayList<Autor> consultarAutores(String prefijo){
+        
+    }
+    
+    public Contenido consultarContenido(Autor autor, Titulo titulo){
+        
+    }
+    
+    public ArrayList<Documento> getDocumentosParecidosFrecs(Documento T, int k){
+    
+    }
+    
+    public ArrayList<Documento> getDocumentosParecidosTfDf(Documento T, int k){
+        
+    }
+    
+    public ArrayList<Documento> getDocumentosBool(ArrayList<String> frase){
+        
+    }
+    
+    
+    public void altaDocumento(Autor autor, Titulo titulo, Contenido contenido) throws ExceptionDomini{
 
         Documento doc;
-        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles);
+        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles); //arreglar
         try{    
-            if (vecDocumentos.contains(doc)){
+            int pos = vecDocumentos.indexOf(doc);
+            if (pos >= 0){
                 throw new ExceptionDomini("El documento ya existe");
             }else{
                 vecDocumentos.add(doc);
                 ++id;
+                ++numDocs;
             }
         } catch(ExceptionDomini e){
             System.out.println(e.getMessage());
@@ -48,16 +77,17 @@ public class CjtoDocumentos {
         vecDocumentos.print();
     }
     
-    public void bajaDocumento(String autor, String titulo, String contenido) throws ExceptionDomini{
+    public void bajaDocumento(Autor autor, Titulo titulo, Contenido contenido) throws ExceptionDomini{
 
         Documento doc;
-        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles);
+        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles); //arreglar + id
         try{    
-            if (!vecDocumentos.contains(doc)){
+            int pos = vecDocumentos.indexOf(doc);
+            if (pos==-1){
                 throw new ExceptionDomini("El documento no existe");
             }else{
-                vecDocumentos.delete(doc);
-                --id;
+                vecDocumentos.remove(pos);
+                --numDocs;
             }
         } catch(ExceptionDomini e){
             System.out.println(e.getMessage());
@@ -65,12 +95,13 @@ public class CjtoDocumentos {
         vecDocumentos.print();
     }
     
-    public void modificaAutorDoc(String autor, String titulo, String contenido) throws ExceptionDomini{
+    public void modificaAutorDoc(Autor autor, Titulo titulo, Contenido contenido, String autorModif) throws ExceptionDomini{
 
         Documento doc;
-        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles);
+        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles); //arreglar + id
         try{    
-            if (!vecDocumentos.contains(doc)){
+            int pos = vecDocumentos.indexOf(doc);
+            if (pos==-1){
                 throw new ExceptionDomini("El documento no existe");
             }else{
                 vecDocumentos.modificarAutor(doc);
@@ -81,12 +112,13 @@ public class CjtoDocumentos {
         vecDocumentos.print();
     }
     
-    public void modificaTituloDoc(String autor, String titulo, String contenido) throws ExceptionDomini{
+    public void modificaTituloDoc(Autor autor, Titulo titulo, Contenido contenido, String TituloModif) throws ExceptionDomini{
 
         Documento doc;
-        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles);
+        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles); //arreglar + id
         try{    
-            if (!vecDocumentos.contains(doc)){
+            int pos = vecDocumentos.indexOf(doc);
+            if (pos==-1){
                 throw new ExceptionDomini("El documento no existe");
             }else{
                 vecDocumentos.modificarTitulo(doc);
@@ -97,12 +129,13 @@ public class CjtoDocumentos {
         vecDocumentos.print();
     }
     
-    public void modificaContenidoDoc(String autor, String titulo, String contenido) throws ExceptionDomini{
+    public void modificaContenidoDoc(Autor autor, Titulo titulo, Contenido contenido, String contenidoModif) throws ExceptionDomini{
 
         Documento doc;
-        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles);
-        try{    
-            if (!vecDocumentos.contains(doc)){
+        doc = new Documento(autor,titulo,contenido,id,español,catalan,ingles); //arreglar + id
+        try{  
+            int pos = vecDocumentos.indexOf(doc);
+            if (pos==-1){
                 throw new ExceptionDomini("El documento no existe");
             }else{
                 vecDocumentos.modificarContenido(doc);
