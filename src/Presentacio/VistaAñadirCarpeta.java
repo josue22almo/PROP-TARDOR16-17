d23b754e608ace0535f71396db232d04ea9bc144
florenciarimolo <florencia.rf96@gmail.com>
2016-12-02 13:48:26 +0100

    Vistas

--- src/Presentacio/VistaAñadirCarpeta.java
+++ src/Presentacio/VistaAñadirCarpeta.java
@@ -5,25 +5,17 @@
  */
 package Presentacio;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
- * @author florenciarf
+ * @author florencia.rimolo
  */
-public class Vista extends javax.swing.JFrame {
+public class VistaAñadirCarpeta extends javax.swing.JFrame {
 
-    private CtrlPresentacio cp;
     /**
-     * Creates new form Vista
-     * @throws java.io.IOException
+     * Creates new form VistaAñadirCarpeta
      */
-    public Vista() throws IOException {
+    public VistaAñadirCarpeta() {
         initComponents();
-        this.cp = new CtrlPresentacio();
-        
     }
 
     /**
@@ -35,52 +27,21 @@ public class Vista extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenuBar2 = new javax.swing.JMenuBar();
-        jMenu2 = new javax.swing.JMenu();
-        jMenu3 = new javax.swing.JMenu();
-        Buscar = new java.awt.Label();
-        jTextField1 = new javax.swing.JTextField();
-        list1 = new java.awt.List();
-        label1 = new java.awt.Label();
+        jFileChooser1 = new javax.swing.JFileChooser();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
 
-        jMenu2.setText("File");
-        jMenuBar2.add(jMenu2);
-
-        jMenu3.setText("Edit");
-        jMenuBar2.add(jMenu3);
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Gestión de documentos");
-        setPreferredSize(new java.awt.Dimension(4000, 3000));
-        getContentPane().setLayout(null);
+        setTitle("Añadir por carpeta...");
 
-        Buscar.setText("Buscar autores:");
-        getContentPane().add(Buscar);
-        Buscar.setBounds(80, 170, 100, 19);
-        Buscar.getAccessibleContext().setAccessibleName("Buscar");
-
-        getContentPane().add(jTextField1);
-        jTextField1.setBounds(200, 170, 170, 19);
-        getContentPane().add(list1);
-        list1.setBounds(80, 260, 210, 290);
-
-        label1.setText("Autores:");
-        getContentPane().add(label1);
-        label1.setBounds(90, 230, 60, 19);
-
-        jMenu1.setText("Archivo");
-
-        jMenuItem2.setText("Añadir por carpeta...");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+        jFileChooser1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
+                jFileChooser1ActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem2);
+
+        jMenu1.setText("Archivo");
 
         jMenuItem1.setText("Cerrar");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
@@ -94,19 +55,33 @@ public class Vista extends javax.swing.JFrame {
 
         setJMenuBar(jMenuBar1);
 
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jFileChooser1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jFileChooser1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
-        System.exit(0);
+        setVisible(false);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+    private void jFileChooser1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFileChooser1ActionPerformed
         // TODO add your handling code here:
-        VistaAñadirCarpeta vc = new VistaAñadirCarpeta();
-        vc.setVisible(true);
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
+        
+    }//GEN-LAST:event_jFileChooser1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -125,39 +100,28 @@ public class Vista extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaAñadirCarpeta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaAñadirCarpeta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaAñadirCarpeta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaAñadirCarpeta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                try {
-                    new Vista().setVisible(true);
-                } catch (IOException ex) {
-                    Logger.getLogger(Vista.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                new VistaAñadirCarpeta().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private java.awt.Label Buscar;
+    private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuBar jMenuBar2;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JTextField jTextField1;
-    private java.awt.Label label1;
-    private java.awt.List list1;
     // End of variables declaration//GEN-END:variables
 }
