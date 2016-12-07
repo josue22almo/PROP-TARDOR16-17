90d5ed69dd479dc611204a2a2f04c2f5d60b4b15
jessicasobreviela <jessi.sobreviela@gmail.com>
2016-12-07 19:53:36 +0100

    falta arreglar el alta de nuestros documentos

--- src/Domini/DriverCtrlDomini.java
+++ src/Domini/DriverCtrlDomini.java
@@ -15,22 +15,22 @@ public class DriverCtrlDomini {
     public static void main(String[] args) throws IOException, Exception{
         CtrlDomini c = new CtrlDomini();
         Scanner sc = new Scanner(System.in);
-        //c.altaConjuntoDocumentosDirectorio("Documentos");
         while(true){
             System.out.println("Indique qué desea hacer:");
             System.out.println("1. Dar de alta a un conjunto de documentos dado un directorio");
-            System.out.println("2. Dar de alta un documento");
-            System.out.println("3. Dar de baja un documento");
-            System.out.println("4. Modificar autor de un documento");
-            System.out.println("5. Modificar titulo de un documento");
-            System.out.println("6. Modificar contenido de un documento");
-            System.out.println("7. Consultar titulos de los documentos de un autor");
-            System.out.println("8. Consultar autores que empiezan por un prefijo");
-            System.out.println("9. Consultar contenido de un documento"); 
-            System.out.println("10. Consultar lista de los k documentos más parecidos a T");
-            System.out.println("11. Consultar lista de los documentos con una expresión booleana");
-            System.out.println("12. Consultar todo el conjunto de documentos");
-            System.out.println("13. Salir");
+            System.out.println("2. Dar de alta nuestro conjunto de documentos");
+            System.out.println("3. Dar de alta un documento");
+            System.out.println("4. Dar de baja un documento");
+            System.out.println("5. Modificar autor de un documento");
+            System.out.println("6. Modificar titulo de un documento");
+            System.out.println("7. Modificar contenido de un documento");
+            System.out.println("8. Consultar titulos de los documentos de un autor");
+            System.out.println("9. Consultar autores que empiezan por un prefijo");
+            System.out.println("10. Consultar contenido de un documento"); 
+            System.out.println("11. Consultar lista de los k documentos más parecidos a T");
+            System.out.println("12. Consultar lista de los documentos con una expresión booleana");
+            System.out.println("13. Consultar todo el conjunto de documentos");
+            System.out.println("14. Salir");
             String autor;
             String titulo;
             String contenido;
@@ -46,6 +46,9 @@ public class DriverCtrlDomini {
                     c.altaConjuntoDocumentosDirectorio(directorio);
                     break;
                 case 2:
+                    c.altaConjuntoDocumentosDirectorio("Documentos");
+                    break;
+                case 3:
                     System.out.println("Alta documento");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -60,7 +63,7 @@ public class DriverCtrlDomini {
                     }
                     c.altaDocumento(autor,titulo,contenido);
                     break;
-                case 3:
+                case 4:
                     System.out.println("Baja documento");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -68,7 +71,7 @@ public class DriverCtrlDomini {
                     titulo = sc.nextLine();
                     c.bajaDocumento(autor,titulo); 
                     break;
-                case 4:
+                case 5:
                     System.out.println("Modificar autor");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -78,7 +81,7 @@ public class DriverCtrlDomini {
                     String autorAux = sc.nextLine();
                     c.modificaAutorDoc(autor,titulo,autorAux); 
                     break;
-                case 5:
+                case 6:
                     System.out.println("Modificar titulo");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -88,7 +91,7 @@ public class DriverCtrlDomini {
                     String tituloAux = sc.nextLine();;
                     c.modificaTituloDoc(autor,titulo,tituloAux); 
                     break;
-                case 6:
+                case 7:
                     System.out.println("Modificar contenido");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -103,13 +106,13 @@ public class DriverCtrlDomini {
                     }
                     c.modificaContenidoDoc(autor,titulo,contAux); 
                     break;
-                case 7:
+                case 8:
                     System.out.println("Consular títulos autor");
                     autor = sc.nextLine();
                     ArrayList<String> tit = c.consultarTitulosAutor(autor); 
                     for (int i=0; i < tit.size(); ++i) System.out.println(tit.get(i));
                     break;
-                case 8:
+                case 9:
                     System.out.println("Consultar autores por prefijo");
                     System.out.println("Prefijo:");
                     String prefijo = "";
@@ -117,7 +120,7 @@ public class DriverCtrlDomini {
                     ArrayList<String> aut = c.consultarAutoresPorPrefijo(prefijo); 
                     for (int i=0; i < aut.size(); ++i) System.out.println(aut.get(i));
                     break;
-                case 9:
+                case 10:
                     System.out.println("Consultar contenido");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -127,7 +130,7 @@ public class DriverCtrlDomini {
                     System.out.println("Contenido:");
                     System.out.println(contenido);
                     break;
-                case 10:
+                case 11:
                     System.out.println("Consultar k mas parecidos a T");
                     System.out.println("Autor:");
                     autor = sc.nextLine();
@@ -145,13 +148,13 @@ public class DriverCtrlDomini {
                         System.out.println();
                     } 
                     break;
-                case 11:
+                case 12:
                     System.out.println("Consultar expresión booleana");
                     System.out.println("Frase booleana:");
                     String booleano = sc.nextLine();
                     c.getDocumentosBool(booleano);
                     break;
-                case 12: 
+                case 13: 
                     System.out.println("Consultar todo el conjunto de documentos");
                     System.out.println("vecDocumentos es:");
                     CjtoDocumentos cd = c.getCjtoDocumentos();
@@ -162,7 +165,7 @@ public class DriverCtrlDomini {
                         Documento doc = vecDocumentos.get(id);
                         System.out.print("Id: " + id + '\n' + "Autor: " + doc.getAutor() + '\n');
                         System.out.println("Titulo: " + doc.getTitulo() + '\n' + "Contenido: " + doc.getContenidoOriginal());
-//                        System.out.println();
+                        //System.out.println();
                     }
                     System.out.println();
                     System.out.println("ids es:");
@@ -176,11 +179,11 @@ public class DriverCtrlDomini {
                             String t = (String) it3.next();
                             System.out.print("Id: " + ids.get(a).get(t) + '\n' + "Autor: " + a + '\n');
                             System.out.println("Titulo: " + t + '\n');
-//                            System.out.println();
+                            //System.out.println();
                         }
                     }            
                     break;
-                case 13:
+                case 14:
                     System.out.println("Salir");
                     return;   
                 default:
