96f5f210792a4f503d31fa8f69c0c39f0c6212a4
jessicasobreviela <jessi.sobreviela@gmail.com>
2016-12-17 16:49:58 +0100

    arreglando detalles

--- src/Persistencia/CtrlPersistencia.java
+++ src/Persistencia/CtrlPersistencia.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -24,8 +23,7 @@ public class CtrlPersistencia {
         this.path = path;
     }*/
 
-    public ArrayList<BufferedReader> leerCarpeta(String rutaCarpeta) throws FileNotFoundException
-    {
+    public ArrayList<BufferedReader> leerCarpeta(String rutaCarpeta) throws FileNotFoundException {
         File directorio;
         File[] archivos;
         ArrayList<BufferedReader> result = new ArrayList<>();
@@ -44,31 +42,28 @@ public class CtrlPersistencia {
         return result;
     }
     
-    public BufferedReader leerDocumento (File archivo) throws FileNotFoundException
-    {
+    public BufferedReader leerDocumento (File archivo) throws FileNotFoundException {
         return new BufferedReader(new FileReader(archivo));
     }
     
-    public ArrayList<String> leerPalabrasFuncionales(String archivo) throws Exception
-    {
-        String cadena;
-        ArrayList<String> pf = new ArrayList<>();
-        try {
-        FileReader f = new FileReader(archivo);
-        if (f == null) throw new Exception("No existe el fichero " + archivo);
-        BufferedReader b = new BufferedReader(f);
-        while((cadena = b.readLine())!=null) {
-            pf.add(cadena);
-        }
-        b.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("El fichero no existe.");
-        }
-        return pf;
+    public ArrayList<String> leerPalabrasFuncionales(String archivo) throws Exception {
+      String cadena;
+      ArrayList<String> pf = new ArrayList<>();
+      try {
+      FileReader f = new FileReader(archivo);
+      if (f == null) throw new Exception("No existe el fichero " + archivo);
+      BufferedReader b = new BufferedReader(f);
+      while((cadena = b.readLine())!=null) {
+          pf.add(cadena);
+      }
+      b.close();
+      } catch (FileNotFoundException e) {
+          System.out.println("El fichero no existe.");
+      }
+      return pf;
     } 
     
-    public void guardarDocumento (String autor, String titulo, String contenido) throws IOException
-    {
+    public void guardarDocumento (String autor, String titulo, String contenido) throws IOException{
         File txt = new File(path, titulo + "," + autor);
         System.out.println(path);
         txt.createNewFile();
@@ -85,17 +80,11 @@ public class CtrlPersistencia {
         }
     }
      
-    
-    public void eliminarDocumento(String autor, String titulo)
-    {
+    public void eliminarDocumento(String autor, String titulo){
         File file = new File(path,titulo + "," + autor);
         if (file.delete())
             System.out.println("El fichero ha sido borrado satisfactoriamente");
         else
             System.out.println("El fichero no puede ser borrado");
     }
-    
-    /*public void cargarDatos() throws Exception{
-        leerCarpeta(path);
-    }*/
 }
