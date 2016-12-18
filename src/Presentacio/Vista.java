a8f9dcd19fd8d5fd0f7edb9a3ec9b97f5dd9d811
jessicasobreviela <jessi.sobreviela@gmail.com>
2016-12-18 13:17:18 +0100

    arreglando detalles

--- src/Presentacio/Vista.java
+++ src/Presentacio/Vista.java
@@ -6,7 +6,6 @@
 package Presentacio;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
--- src/Presentacio/VistaModificarTitulo.java
+++ src/Presentacio/VistaModificarTitulo.java
@@ -6,8 +6,6 @@
 package Presentacio;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
